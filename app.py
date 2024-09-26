72e14d60692ac933ea0c55f535a12f81c8acb913
Fernando <fernandocebrito@gmail.com>
2024-09-26 09:04:43 -0300

    Adição do banco de dados

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ load_dotenv()
 app = Flask(__name__)
 
 # Configuração do banco de dados SQLite
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
